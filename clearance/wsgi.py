333e0dce7d7cd07541f697a7b888c2dc5b9d24f6
Deepak Bhattachar <bhattachar@gmail.com>
2015-05-18 21:50:00 -0600

    updated wsgi

--- clearance/wsgi.py
+++ clearance/wsgi.py
@@ -8,7 +8,15 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wowclearance.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clearance.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+try:
+	from dj_static import Cling
+
+	application = Cling(get_wsgi_application())
+
+except:
+	pass
